b568cc823e612a0f37e92d0b18511f9a65e8dfaf
Bharathpeddi18 <peddibharath07@gmail.com>
2026-09-25 12:41:43 +0530

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,5 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#00308F',
+    icons: [
+      {
+        src: '/icons/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
   };
 }
